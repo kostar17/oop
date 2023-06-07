62fa7bfb7b1e60669bdc707cad931287be3e6706
kostar17 <127311995+kostar17@users.noreply.github.com>
2023-06-07 22:54:47 +0900

    Update sprint4.cpp

--- sprint4.cpp
+++ sprint4.cpp
@@ -1,9 +1,10 @@
 #include "sprint4.h"
 
 int main() {
-	int time = 0, a = 0;
+	int time = 0, a = 0, Max = 0;
+	float b = 0;
 	queue* aa;
-	aa = new queue(15, 10, 200);
+	aa = new queue(30, 10, 200);
 	int NofPackets[120] = {};
 	ofstream xx("data4.txt");
 	while (time < 120) {
@@ -11,10 +12,20 @@ int main() {
 		aa->put_out();
 		NofPackets[a] = aa->q.size();
 		xx << NofPackets[a] << endl;
-		Sleep(1000);
+		Sleep(10);
+		b += int(NofPackets[a]);
 		a++;
 		time++;
 	}
+	for (int i = 0; i < 120; i++) {
+		if (NofPackets[i] > Max) {
+			Max = NofPackets[i];
+		}
+	}
+	b = b / 120;
+	cout << "Average: " << b << endl;
+	cout << "Maximum: " << Max << endl;
+	cout << "Lost: " << aa->lost << endl;
 	xx.close();
 	return 123;
 }
